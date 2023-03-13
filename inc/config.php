e5ebf58f6484e93bcbf5e8cfbb8677ffbacd7087
Luis Rohden <luisrohden@gmail.com>
2023-03-13 16:04:23 -0300

    Update config.php
    Add seique.com.br credentials

--- inc/config.php
+++ inc/config.php
@@ -3,9 +3,21 @@ const twigConstants =  [
 	'projectURL' => '/'
 ];
 const FilesDirectory = './files/';
+/*
 const mysqlAcess = [
 	'db_name' => 'expertshop',
 	'db_host' => 'localhost',
 	'db_user' => 'root',
 	'db_password' => '',
 ];
+*/
+
+#seique.com.br
+
+const mysqlAcess = [
+	'db_name' => 'expert',
+	'db_host' => 'mysql834.umbler.com',
+	'db_user' => 'luisrohden',
+	'db_password' => '8X_U-9l-Rxqh?',
+];
+
